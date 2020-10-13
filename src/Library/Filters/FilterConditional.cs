407740ec54321a48c9743f96af6caae8719bc6e4
EstefanoGiusiano <estefanogiusiano2607@gmail.com>
2020-10-13 11:23:41 -0300

    Ejercicio 4 _ prueba 1

--- src/Library/Filters/FilterConditional.cs
+++ src/Library/Filters/FilterConditional.cs
@@ -1,33 +1,40 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Text;
-// using CompAndDel;
-// using System.Drawing;
-// using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CompAndDel;
+using System.Drawing;
+using System.Diagnostics; 
+using TwitterUCU;
 
-// namespace CompAndDel.Filters
-// {
-//     public class FilterConditional : IFilter
-//     {
-//         /// <summary>
-//         /// Recibe una imagen y la retorna con un filtro del tipo inventado aplicado
-//         /// </summary>
-//         /// <param name="image">Imagen a la cual se le va a plicar el filtro.</param>
-//         /// <returns>Imagen con el filtro aplicado</returns>
-//         public IPicture Filter(IPicture image)
-//         {
-//             Debug.Assert(image != null);
-//             IPicture invento = image.Clone();
+namespace CompAndDel.Filters
+{
+    public class FilterConditional : IFilter
+    {
+        /// <summary>
+        /// Recibe una imagen y la retorna con un filtro del tipo inventado aplicado
+        /// </summary>
+        /// <param name="image">Imagen a la cual se le va a plicar el filtro.</param>
+        /// <returns>Imagen con el filtro aplicado</returns>
+        public IPicture Filter(IPicture image)
+        {
+            Debug.Assert(image != null);
+            IPicture invento = image.Clone();
+            PictureProvider pic = new PictureProvider();
+            pic.SavePicture(invento, $@"./../../images/bill{FilterSave.Instance.Count}.jpg");
 
-//             if (CognitiveApi(image))
-//             {
-//                 //FILTRO 1
-//             }
-//             else
-//             {
-//                 // FILTRO 2
-//             }
-//             return invento;
-//         }
-//     }
-// }
+            if (CognitiveApi(pic))
+            {
+                
+                Console.WriteLine("ESTO ES UN FILTRO", $@"./../../images/bill{FilterSave.Instance.Count}.jpg");
+                
+
+            }
+            else
+            {
+                pic.SavePicture(invento, $@"./../../images/bill{FilterSave.Instance.Count}.jpg");
+                //Console.WriteLine(invento.PublishToTwitter("ESTO ES UN FILTRO", $@"./../../images/bill{FilterSave.Instance.Count}.jpg"));
+            }
+            return invento;
+        }
+    }
+}
